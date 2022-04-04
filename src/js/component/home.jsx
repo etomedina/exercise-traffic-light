279922a90d1070981411b51ca931dc4c38256618
etomedina <hmedina@LAPTOP-84M4TVEB.localdomain>
2022-04-03 22:44:29 -0400

    Fix troubles

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,31 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
 
 //create your first component
-const Home = () => {
+const TrafficTop = () => {
+	const [onColor, setOnColor] = useState();
 	return (
-		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			<div className="box">
+				<div
+					onClick={(e) => setOnColor("red")}
+					className={
+						"red-ligh" + (onColor === "red" ? " brillo" : "")
+					}></div>
+
+				<div
+					onClick={(e) => setOnColor("yellow")}
+					className={
+						"yellow-ligh" + (onColor === "yellow" ? " brillo" : "")
+					}></div>
+
+				<div
+					onClick={(e) => setOnColor("green")}
+					className={
+						"green-ligh" + (onColor === "green" ? " brillo" : "")
+					}></div>
+			</div>
 		</div>
 	);
 };
 
-export default Home;
+export default TrafficTop;
